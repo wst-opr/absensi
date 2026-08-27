782d3f0d962f5772164b1c802a65f84a8fe06efc
wst-opr <bprwingsati.operasional@gmail.com>
2026-08-27 14:28:37 +0700

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -21,7 +21,7 @@ const firebaseConfig = {
 //  >>> PASTIKAN SAMA PERSIS DENGAN daftar di firestore.rules (function adminEmails) <<<
 // ============================================================
 const ADMIN_EMAILS = [
-    // "admin@wingsati.com",
+    // "bprwingsati.operasional@gmail.com",
     // "dirut@wingsati.com"
 ];
 
